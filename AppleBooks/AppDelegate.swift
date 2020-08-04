9976d9a620a91971c1c96d469a1d6fd90e781f19
ken <kyereme@hotmail.co.uk>
2020-08-04 23:05:46 +0100

    AppleBooksV2

--- AppleBooks/AppDelegate.swift
+++ AppleBooks/AppDelegate.swift
@@ -11,10 +11,15 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        
         return true
     }
 
